48890d120c4182a226bf26537fdcc0e8fb38300d
Aman Kumar <amancadet98@gmail.com>
2021-07-02 19:50:27 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (72.95%) - LeetHub

--- subsets-ii/subsets-ii.cpp
+++ subsets-ii/subsets-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-        
-        int n = nums.size();
-        int cnt_subsets = (1<<n);
-        sort(nums.begin(),nums.end());
-        set<vector<int>> s;
-        
-        for (int i =0 ;i<cnt_subsets ; i++)
+        void help(vector<int> &nums,int start,vector<int> &bag,vector<vector<int>> &ans)
+    {
+        //base case
+        if(start==nums.size())
         {
-            vector<int> subset;
-          for (int j = 0 ;j<n ;j++)
-          {
-              if ( (i & (1<<j) )!=0)
-              {
-                  subset.push_back(nums[j]);
-              }
-          }
-            s.insert(subset);
+            ans.push_back(bag);
+            return;
         }
+        //rec step
         
-        vector<vector<int>> ans(s.begin(), s.end());
-        
+        //item include
+        bag.push_back(nums[start]);
+        help(nums,start+1,bag,ans);
+        bag.pop_back();
+        //item not include
+        int j=start+1;
+        while(j<nums.size() && nums[j]==nums[start])j++;
+            
+        help(nums,j,bag,ans);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> bag;
+        help(nums,0,bag,ans);
         return ans;
     }
 };
